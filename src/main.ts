7161068107744f08e2a4a6eb2a3964281a91fb90
Rodrigo Villarreal <villarrealrodrigo.n@gmail.com>
2025-12-26 16:40:11 -0300

    fix for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+
+const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser()); // ✅ cookies
+  app.use(cookieParser());
+
   app.enableCors({
-    origin: ['http://localhost:3001'], // tu Next
+    origin: ['http://localhost:3001'],
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
