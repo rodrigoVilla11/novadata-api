8146153264f7d04321d586fb789dc9f4cd29b43d
Rodrigo Villarreal <villarrealrodrigo.n@gmail.com>
2026-01-09 00:05:57 -0300

    HOT FIX:improve multitenant on operative modules

--- src/production/production.module.ts
+++ src/production/production.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductionController } from './production.controller';
 import { ProductionService } from './production.service';
 import { ProductionEntry, ProductionSchema } from './schemas/production.schema';
+import { Employee, EmployeeSchema } from 'src/employees/schemas/employee.schema';
+import { Task, TaskSchema } from 'src/tasks/schemas/task.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ProductionEntry.name, schema: ProductionSchema },
+      { name: Employee.name, schema: EmployeeSchema },
+      { name: Task.name, schema: TaskSchema },
     ]),
   ],
   controllers: [ProductionController],
